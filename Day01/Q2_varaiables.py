699cefd0be1ab255cae59cbab45fdec2675d2a3e
GIRIMAHENDRAPPA <girimahendra7989@gmail.com>
2026-09-16 22:28:26 +0530

    Add personal information variables and print statements

---
+++ Day01/Q2_varaiables.py
@@ -0,0 +1,14 @@
+my_name="Giri Mahendra"
+my_branch="M.Tech AIML"  
+my_college="LPU"
+my_age=22
+my_passout_year=2028
+my_cgpa=8.09
+is_placed=False
+print(f"name:{my_name}"))
+print(f"branch:{my_branch}"))
+print(f"college:{my_college}"))
+print(f"Age:{my_age}"))
+print(f"CGPA:{my_cgpa}"))
+print(f"placed:{is_placed}"))
+
