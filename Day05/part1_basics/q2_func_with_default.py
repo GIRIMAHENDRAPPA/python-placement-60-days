4465fcb605d3584cbe57628b0bb578eac40d66fa
giri <giri@gmail.com>
2026-09-20 12:20:47 +0530

    Delete Day05

--- Day05/part1_basics/q2_func_with_default.py
+++
@@ -1,7 +0,0 @@
-def placement_status(cgpa,attendance=75):
-    if cgpa >= 7 and attendance > 75:
-        return "Eligible for placement"
-    return "not placed"
-
-print(placement_status(8, 90))  # Eligible for placement
-print(placement_status(6, 80))  # not placed
--- Day05/part2_programs/q3_second_largest.py
+++
@@ -1,5 +0,0 @@
-def second_largest(arr):
-    arr = list(set(arr))  # Remove duplicates
-    arr.sort()
-    return arr[-2]
-print(second_largest([1, 2, 3, 4, 5]))
